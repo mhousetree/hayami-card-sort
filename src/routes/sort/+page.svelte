c5d4aa8b1e3315ed7a383292e8afc8fea1491aa4
mhousetree <mhousetree@gmail.com>
2023-05-25 02:54:20 +0900

    Implement sort page

--- src/routes/sort/+page.svelte
+++ src/routes/sort/+page.svelte
@@ -1,68 +1,121 @@
 <script lang="ts">
-	import { ss } from '../../data/ss';
+	import { ssCards } from '../../data/ss';
 	import Sorter from '../../modules/Sorter';
-	const sort = new Sorter(ss);
+	const sort = new Sorter(Object.keys(ssCards));
 	let result = sort.sort();
 </script>
 
 {#if result}
-	<h1>結果発表</h1>
-	<ul>
-		{#each ss as item}
-			<li>{item}</li>
-		{/each}
-	</ul>
-	{sort.array}
-	<a href="/">トップに戻る</a>
+	<section id="result">
+		<h1>結果発表</h1>
+		<ol>
+			{#each sort.array as item}
+				<li>{ssCards[item]}</li>
+			{/each}
+		</ol>
+		<a href="/">トップに戻る</a>
+	</section>
 {:else}
-	<h1>ソート</h1>
-	<button
-		on:click={() => {
-			sort.backable = true;
-			sort.prevItems = JSON.parse(JSON.stringify(sort.items));
-			sort.addResult(sort.lastChallenge[0], sort.lastChallenge[1]);
-			result = sort.sort();
-		}}
-	>
-		{sort.lastChallenge[0]}
-	</button>
-	<button
-		on:click={() => {
-			sort.backable = true;
-			sort.prevItems = JSON.parse(JSON.stringify(sort.items));
-			sort.addResult(sort.lastChallenge[1], sort.lastChallenge[0]);
-			result = sort.sort();
-		}}
-	>
-		{sort.lastChallenge[1]}
-	</button>
-	<button
-		on:click={() => {
-			sort.backable = true;
-			sort.prevItems = JSON.parse(JSON.stringify(sort.items));
-			sort.addEqual(sort.lastChallenge[0], sort.lastChallenge[1]);
-			result = sort.sort();
-		}}
-	>
-		両方勝ち
-	</button>
-	<button
-		on:click={() => {
-			if (sort.backable) {
-				sort.back();
-				result = sort.sort();
-			} else {
-				alert('これ以上戻れません！');
-			}
-		}}
-	>
-		ひとつ戻る
-	</button>
-	<!-- <button
-		on:click={() => {
-			result = true;
-		}}
-	>
-		ソート完了
-	</button> -->
+	<section id="sort">
+		<h1>ソート</h1>
+		<p>ラウンド{sort.currentRound} - {sort.progress}%</p>
+		<section id="choices">
+			<button
+				id="left-choice"
+				on:click={() => {
+					sort.backable = true;
+					sort.prevItems = JSON.parse(JSON.stringify(sort.items));
+					sort.addResult(sort.lastChallenge[0], sort.lastChallenge[1]);
+					result = sort.sort();
+				}}
+			>
+				<img src={`./assets/ss/${sort.lastChallenge[0]}.jpeg`} alt="" />
+				<h2>
+					{ssCards[sort.lastChallenge[0]]}
+				</h2>
+			</button>
+			<button
+				id="right-choice"
+				on:click={() => {
+					sort.backable = true;
+					sort.prevItems = JSON.parse(JSON.stringify(sort.items));
+					sort.addResult(sort.lastChallenge[1], sort.lastChallenge[0]);
+					result = sort.sort();
+				}}
+			>
+				<img src={`./assets/ss/${sort.lastChallenge[1]}.jpeg`} alt="" />
+				<h2>
+					{ssCards[sort.lastChallenge[1]]}
+				</h2>
+			</button>
+			<button
+				id="both-win"
+				on:click={() => {
+					sort.backable = true;
+					sort.prevItems = JSON.parse(JSON.stringify(sort.items));
+					sort.addEqual(sort.lastChallenge[0], sort.lastChallenge[1]);
+					result = sort.sort();
+				}}
+			>
+				両方勝ち
+			</button>
+			<!-- <button
+			on:click={() => {
+				if (sort.backable) {
+					sort.back();
+					result = sort.sort();
+				} else {
+					alert('これ以上戻れません！');
+				}
+			}}
+		>
+			ひとつ戻る
+		</button> -->
+		</section>
+	</section>
 {/if}
+
+<style lang="scss">
+	#sort {
+		text-align: center;
+		margin-top: 4rem;
+	}
+	#choices {
+		display: grid;
+		width: 100%;
+		grid-template-columns: repeat(2, 1fr);
+		gap: 0.5rem;
+
+		button {
+			border: 1px solid #0001;
+			background-color: #fff;
+			box-shadow: 0 2px 10px #0001;
+			border-radius: 4px;
+			padding: 1rem;
+			font-size: 1.2rem;
+			transition: .2s;
+
+			&:hover {
+				translate: 0 -2px;
+				box-shadow: 0 4px 15px #6661;
+			}
+
+			h2 {
+				font-size: 1em;
+				margin: 0;
+				margin-top: .8rem;
+			}
+
+			img {
+				width: 100%;
+				display: block;
+				aspect-ratio: 45/29;
+				object-fit: contain;
+			}
+		}
+
+		#both-win {
+			grid-column: 1 / -1;
+		}
+	}
+</style>
