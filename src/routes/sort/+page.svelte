86508f1796d5368e90833da41ba56fa6c8e30d5f
mhousetree <mhousetree@gmail.com>
2023-05-25 22:59:27 +0900

    Update result page

--- src/routes/sort/+page.svelte
+++ src/routes/sort/+page.svelte
@@ -1,23 +1,63 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
 	import { ssCards } from '../../data/ss';
 	import Sorter from '../../modules/Sorter';
 	const sort = new Sorter(Object.keys(ssCards));
-	let result = sort.sort();
+	// let result = sort.sort();
+	let result = true;
+
+	onMount(() => {
+		// overflow: hidden
+		// @ts-ignore
+		document.querySelector('body').style.overflow = 'scroll';
+	});
+
+	const getTweetLink = (one: string, two: string, three: string) => {
+		// https://tools.ikunaga.net/tweet-link/
+		return `https://twitter.com/intent/tweet?text=%E7%A7%81%E3%81%AE%E9%80%9F%E6%B0%B4%E5%A5%8F%E3%81%95%E3%82%93%E3%81%AE%E3%82%AB%E3%83%BC%E3%83%89%E3%83%A9%E3%83%B3%E3%82%AD%E3%83%B3%E3%82%B0TOP3%E3%81%AF%E3%80%81%0A1.%20${one}%0A2.%20${two}%0A3.%20${three}%0A%E3%81%A7%E3%81%97%E3%81%9F%EF%BC%81%0A%0A%E9%80%9F%E6%B0%B4%E5%A5%8F%E3%81%95%E3%82%93%E3%82%AB%E3%83%BC%E3%83%89%E3%82%A4%E3%83%A9%E3%82%B9%E3%83%88%E3%82%BD%E3%83%BC%E3%83%88%E3%81%A7%E9%81%8A%E3%82%93%E3%81%A7%E3%81%BF%E3%82%88%E3%81%86%EF%BC%81%0Ahttps%3A%2F%2Fhayami-card-sort.vercel.app%2F`;
+	};
 </script>
 
 {#if result}
 	<section id="result">
 		<h1>結果発表</h1>
-		<ol>
-			{#each sort.array as item}
-				<li>{ssCards[item]}</li>
+		<ol id="top3">
+			{#each sort.array as item, i}
+				{#if i < 3}
+					<li>
+						<img src={`./assets/ss/${item}.jpeg`} alt={ssCards[item]} />
+						<p>
+							{ssCards[item]}
+						</p>
+					</li>
+				{/if}
 			{/each}
 		</ol>
-		<a href="/">トップに戻る</a>
+		<div id="over4">
+			<ol id="over4-first">
+				{#each sort.array as item, i}
+					{#if i > 2 && i < (sort.array.length - 3) / 2 + 3}
+						<li>{ssCards[item]}</li>
+					{/if}
+				{/each}
+			</ol>
+			<ol id="over4-second">
+				{#each sort.array as item, i}
+					{#if i > (sort.array.length - 3) / 2 + 3}
+						<li>{ssCards[item]}</li>
+					{/if}
+				{/each}
+			</ol>
+		</div>
+		<a
+			id="tweetbutton"
+			href={getTweetLink(ssCards[sort.array[0]], ssCards[sort.array[1]], ssCards[sort.array[2]])}
+			target="_blank"><img src="./twitter.png" alt="tweetbutton" />ツイートする</a
+		>
+		<a id="to_top" href="/">トップに戻る</a>
 	</section>
 {:else}
 	<section id="sort">
-		<h1>ソート</h1>
 		<p>ラウンド{sort.currentRound} - {sort.progress}%</p>
 		<section id="choices">
 			<button
@@ -57,28 +97,23 @@
 					result = sort.sort();
 				}}
 			>
-				両方勝ち
+				同じぐらい好き
 			</button>
-			<!-- <button
-			on:click={() => {
-				if (sort.backable) {
-					sort.back();
-					result = sort.sort();
-				} else {
-					alert('これ以上戻れません！');
-				}
-			}}
-		>
-			ひとつ戻る
-		</button> -->
 		</section>
 	</section>
 {/if}
 
 <style lang="scss">
 	#sort {
+		box-sizing: border-box;
+		height: 100vh;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
 		text-align: center;
-		margin-top: 4rem;
+		margin: 0 auto;
+		padding-bottom: 2rem;
+		width: min(90%, 960px);
 	}
 	#choices {
 		display: grid;
@@ -92,7 +127,7 @@
 			box-shadow: 0 2px 10px #0001;
 			border-radius: 4px;
 			padding: 1rem;
-			font-size: 1.2rem;
+			font-size: 2vw;
 			transition: 0.2s;
 
 			&:hover {
@@ -116,6 +151,174 @@
 
 		#both-win {
 			grid-column: 1 / -1;
+		}
+	}
+
+	#result {
+		width: min(80%, 800px);
+		margin: 0 auto;
+		font-size: min(1rem, 1.9vw);
+
+		h1 {
+			text-align: center;
+		}
+
+		#top3 {
+			width: 100%;
+			display: grid;
+			grid-template-columns: repeat(2, 1fr);
+			column-gap: 2rem;
+			counter-reset: item;
+			margin: 0;
+
+			li:first-of-type {
+				grid-column: 1 / -1;
+				width: 80%;
+				justify-self: center;
+
+				p::before {
+					background-color: #cc9f52;
+				}
+			}
+
+			li:nth-of-type(2) p::before {
+				background-color: #7d908b;
+			}
+
+			li:nth-of-type(3) p::before {
+				background-color: #a86539;
+			}
+
+			li {
+				display: flex;
+				flex-direction: column;
+				align-items: center;
+				padding-left: 0;
+
+				&::before {
+					content: none;
+				}
+				p {
+					width: fit-content;
+					position: relative;
+					padding-left: 2.5em;
+				}
+				p::before {
+					position: absolute;
+					left: 0;
+					display: block;
+					counter-increment: item;
+					content: counter(item);
+					width: 2em;
+					height: 2em;
+					text-align: center;
+					line-height: 2em;
+					border-radius: 50%;
+					color: #fff;
+				}
+			}
+		}
+
+		#over4 {
+			display: flex;
+			justify-content: space-between;
+		}
+
+		ol {
+			list-style-type: none;
+			padding: 0;
+
+			li {
+				position: relative;
+				padding-left: 2.5em;
+				line-height: 2em;
+				margin-bottom: 1em;
+			}
+
+			li::before {
+				position: absolute;
+				left: 0;
+				display: block;
+				counter-increment: item;
+				content: counter(item);
+				background-color: #ddd;
+				width: 2em;
+				height: 2em;
+				text-align: center;
+				line-height: 2em;
+				border-radius: 50%;
+			}
+
+			li:nth-of-type(n + 4) {
+				grid-column: 1 / -1;
+			}
+
+			img {
+				width: 100%;
+			}
+		}
+	}
+
+	#tweetbutton {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		text-decoration: none;
+
+		img {
+			width: 64px;
+			margin-bottom: 0.5rem;
+		}
+	}
+
+	#to_top {
+		position: relative;
+		display: block;
+		width: fit-content;
+		margin: 2rem auto 2rem;
+		padding: 0.5rem 0.7rem 0.5rem 1rem;
+		border-radius: 4px;
+		letter-spacing: 0.1rem;
+		text-decoration: none;
+		background-color: #333772;
+		color: #fbffcf;
+		overflow: hidden;
+
+		&::after {
+			content: '';
+			position: absolute;
+			display: block;
+			width: 3rem;
+			height: 3rem;
+			top: -0.3rem;
+			right: -4.5rem;
+			transform: skew(-45deg);
+			background-color: #666;
+			mix-blend-mode: overlay;
+		}
+
+		&:hover {
+			animation: blink 0.8s ease-in-out 0s infinite alternate both;
+			@keyframes blink {
+				from {
+					background-color: #333772;
+				}
+				to {
+					background-color: #494d90;
+				}
+			}
+
+			&:after {
+				animation: shine 0.8s ease-in-out 0s infinite normal both;
+				@keyframes shine {
+					from {
+						right: -4.5rem;
+					}
+					to {
+						right: 9rem;
+					}
+				}
+			}
 		}
 	}
 </style>
