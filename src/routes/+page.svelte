661077c136b0098246fb4229d8b1cec4464f7f57
mhousetree <mhousetree@gmail.com>
2023-05-25 22:59:03 +0900

    Add DOF

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,17 @@
 	import { onMount } from 'svelte';
 	import * as THREE from 'three';
 	import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+	import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer';
+	import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass';
+	import { BokehPass } from 'three/examples/jsm/postprocessing/BokehPass';
+	import { SMAAPass } from 'three/examples/jsm/postprocessing/SMAAPass';
 	import { ssCards } from '../data/ss';
 
 	onMount(() => {
+		// overflow: hidden
+		// @ts-ignore
+		document.querySelector('body').style.overflow = 'hidden';
+
 		// リサイズに対応
 		const onResize = () => {
 			const width = window.innerWidth;
@@ -26,6 +34,7 @@
 		}
 		const renderer = new THREE.WebGLRenderer({
 			canvas: target,
+			alpha: true,
 			antialias: true
 		});
 		renderer.setPixelRatio(window.innerWidth / window.innerHeight);
@@ -33,6 +42,8 @@
 
 		// シーンを作成
 		const scene = new THREE.Scene();
+
+		scene.background = new THREE.Color(0xffffff);
 
 		// カメラを作成
 		const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight);
@@ -51,7 +62,8 @@
 			const geometry = new THREE.PlaneGeometry(160, 103);
 			const loader = new THREE.TextureLoader();
 			const texture = loader.load(`./assets/ss/${Object.keys(ssCards)[i]}.jpeg`);
-			const material = new THREE.MeshBasicMaterial({ map: texture });
+
+			const material = new THREE.MeshStandardMaterial({ map: texture });
 			const mesh = new THREE.Mesh(geometry, material);
 
 			const radian = (i / 10) * Math.PI * 2;
@@ -66,7 +78,7 @@
 			const geometry = new THREE.PlaneGeometry(160, 103);
 			const loader = new THREE.TextureLoader();
 			const texture = loader.load(`./assets/ss/${Object.keys(ssCards)[i]}.jpeg`);
-			const material = new THREE.MeshBasicMaterial({ map: texture });
+			const material = new THREE.MeshStandardMaterial({ map: texture });
 			const mesh = new THREE.Mesh(geometry, material);
 
 			const radian = (i / 10) * Math.PI * 2;
@@ -81,7 +93,7 @@
 			const geometry = new THREE.PlaneGeometry(160, 103);
 			const loader = new THREE.TextureLoader();
 			const texture = loader.load(`./assets/ss/${Object.keys(ssCards)[i]}.jpeg`);
-			const material = new THREE.MeshBasicMaterial({ map: texture });
+			const material = new THREE.MeshStandardMaterial({ map: texture });
 			const mesh = new THREE.Mesh(geometry, material);
 
 			const radian = (i / 10) * Math.PI * 2;
@@ -91,11 +103,35 @@
 			group3.add(mesh);
 		}
 
+		const ambientLight = new THREE.AmbientLight(0xffffff);
+		scene.add(ambientLight);
+
 		const orbitControls = new OrbitControls(camera, renderer.domElement);
+		orbitControls.maxPolarAngle = Math.PI / 2;
+		orbitControls.minPolarAngle = Math.PI / 2;
 		orbitControls.enableDamping = true;
 		orbitControls.dampingFactor = 0.2;
 		orbitControls.enablePan = false;
 		orbitControls.enableZoom = false;
+
+		const composer = new EffectComposer(renderer);
+
+		const renderPass = new RenderPass(scene, camera);
+		composer.addPass(renderPass);
+
+		const bokehPass = new BokehPass(scene, camera, {
+			focus: 510,
+			aperture: 0.0001,
+			maxblur: 0.003
+		});
+
+		let aaPass = new SMAAPass(
+			window.innerWidth * renderer.getPixelRatio(),
+			window.innerHeight * renderer.getPixelRatio()
+		);
+		aaPass.renderToScreen = true;
+		composer.addPass(aaPass);
+		composer.addPass(bokehPass);
 
 		tick();
 
@@ -106,8 +142,7 @@
 			group1.rotation.y += 0.0006;
 			group2.rotation.y -= 0.0006;
 			group3.rotation.y += 0.0006;
-			renderer.render(scene, camera); // レンダリング
-
+			composer.render();
 			requestAnimationFrame(tick);
 		}
 	});
@@ -116,33 +151,126 @@
 <div id="top">
 	<section>
 		<h1>速水奏さんカードイラストソート</h1>
-		<p>速水奏さんカードイラストソートのトップページです。</p>
-		<a href="/sort">始める</a>
+		<p>
+			速水奏さんのカードイラストを比較して、好きな方を選んでください。<br />
+			最後まで遊ぶと全カードのランキングが見られます。<br />
+			あなたが速水奏さんのどんな部分が好きなのか向き合ってみましょう。
+		</p>
+		<a href="/sort">はじめる！</a>
 	</section>
 
 	<canvas id="myCanvas" />
+	<div />
+
+	<small>
+		2023 &copy; はやかわめぐる<br />
+		当コンテンツ内の画像およびカードタイトルの著作権はゲーム提供元に帰属します。<br />
+		問題が発生した場合は速やかに当コンテンツの公開を終了します。
+	</small>
 </div>
 
 <style lang="scss">
 	#top {
 		position: relative;
+		width: 100%;
+		height: 100vh;
+
+		section {
+			position: absolute;
+			top: 50%;
+			left: 50%;
+			transform: translate(-50%, -50%);
+			text-align: center;
+			z-index: 2;
+			width: max-content;
+			filter: drop-shadow(0 0 3px #fff) drop-shadow(0 0 10px #fff) drop-shadow(0 0 30px #fff)
+				drop-shadow(0 0 50px #fff);
+
+			h1 {
+				font-size: min(4vw, 3rem);
+				margin-bottom: 1.5rem;
+			}
+
+			a {
+				position: relative;
+				display: block;
+				width: fit-content;
+				margin: 2rem auto 0;
+				padding: 0.5rem 0.7rem 0.5rem 1rem;
+				border-radius: 4px;
+				font-size: 1.2rem;
+				letter-spacing: 0.1rem;
+				text-decoration: none;
+				background-color: #333772;
+				color: #fbffcf;
+				overflow: hidden;
+
+				&::after {
+					content: '';
+					position: absolute;
+					display: block;
+					width: 3rem;
+					height: 3rem;
+					top: -0.3rem;
+					right: -4.5rem;
+					transform: skew(-45deg);
+					background-color: #666;
+					mix-blend-mode: overlay;
+				}
+
+				&:hover {
+					animation: blink 0.8s ease-in-out 0s infinite alternate both;
+					@keyframes blink {
+						from {
+							background-color: #333772;
+						}
+						to {
+							background-color: #494d90;
+						}
+					}
+
+					&:after {
+						animation: shine 0.8s ease-in-out 0s infinite normal both;
+						@keyframes shine {
+							from {
+								right: -4.5rem;
+							}
+							to {
+								right: 9rem;
+							}
+						}
+					}
+				}
+			}
+		}
 	}
 
 	#myCanvas {
 		position: absolute;
 		top: 0;
 		left: 0;
+
+		+ div {
+			content: '';
+			display: block;
+			position: absolute;
+			top: 0;
+			left: 0;
+			width: 100vw;
+			height: 100vh;
+			background: radial-gradient(transparent 50%, #ccc);
+		}
 	}
 
-	section {
+	small {
+		font-size: 0.8rem;
 		position: absolute;
-		top: 0;
-		left: 0;
-
+		bottom: 1rem;
+		left: 50%;
 		z-index: 2;
-
-		> * {
-			color: #fff;
-		}
+		translate: -50% 0;
+		text-align: center;
+		width: max-content;
+		color: #888;
 	}
 </style>
